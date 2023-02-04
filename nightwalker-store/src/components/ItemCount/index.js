1b09ecdbb54164eec9684ceb2a9eb34b111540ab
leticiaspcruz <leticiaspcruz@gmail.com>
2023-02-04 19:00:13 -0300

    adding itemCounter with logic

--- nightwalker-store/src/components/ItemCount/index.js
+++ nightwalker-store/src/components/ItemCount/index.js
@@ -1,19 +1,38 @@
 import React, { useState } from "react";
-import {} from './styles';
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { AiOutlineMinusCircle } from "react-icons/ai";
+import { ItemCounterContainer } from './styles';
 
-const ItemCount = ({ stock, initial,  onAdd }) => {
+export const ItemCount = ({ stock, initial= 1 }) => {
   const [count, setCount] = useState(initial);
+  const handleStock = () => {
+    setCount(count + 1);
+    if(count >= stock) {
+      setCount(initial);
+      return alert(`O estoque disponível é de ${stock}`);
+    }
+  }
+  const removeItem = () => {
+    setCount(count - 1);
+    if(count === 0) {
+      setCount(count)
+      return alert('Item removido')
+    }
+  };
+
   return (
     <div>
       <div>
         <h1>contador</h1>
+        <ItemCounterContainer>
+        <button onClick={() => removeItem()}>
+          < AiOutlineMinusCircle />
+        </button>
         <p>{count}</p>
-        <button onClick={() => setCount(count - 1)}>
-          diminuir
+        <button onClick={() => handleStock()}>
+          <IoMdAddCircleOutline />
         </button>
-        <button onClick={() => setCount(count - 1)}>
-          adicionar
-        </button>
+        </ItemCounterContainer>
       </div>
       <button onClick={() => console.log('adicionou no carrinho')}>
           adicionar ao carrinho
---
+++ nightwalker-store/src/components/ItemCount/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ItemCounterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin: 12px 0px;
+`;
