0e8f5a4b2e131857832218cdf9a0f8b584ee2fa8
leticiaspcruz <leticiaspcruz@gmail.com>
2023-03-02 21:00:25 -0300

    config firebase

--- nightwalker-store/src/index.js
+++ nightwalker-store/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcTukZ8V6kAfmY6Ul45izwLgTMsS0zCyw",
+  authDomain: "nightwalker-store.firebaseapp.com",
+  projectId: "nightwalker-store",
+  storageBucket: "nightwalker-store.appspot.com",
+  messagingSenderId: "702155068829",
+  appId: "1:702155068829:web:b1e86d5ee5ad6830a70b74",
+  measurementId: "G-T3872E6GHM"
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
